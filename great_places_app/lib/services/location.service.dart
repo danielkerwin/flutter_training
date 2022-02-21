c0234e213645dd9e534cb762808aa59472616efb
Daniel Kerwin <dkerwin@me.com>
2022-02-22 10:38:49 +1100

    chore: updates

--- great_places_app/lib/services/location.service.dart
+++ great_places_app/lib/services/location.service.dart
@@ -1,9 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
 class LocationApi {
   static staticApi(double lat, double long) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$lat,$long&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C$lat,$long';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C$lat,$long';
   }
 }
 
